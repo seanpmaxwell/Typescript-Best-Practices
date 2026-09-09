b63617ed28f46ee2dae44ec12cbd27106cf78557
Sean Maxwell <10490175+seanpmaxwell@users.noreply.github.com>
2026-09-09 10:48:31 -0700

    Update and rename classes-vs-closure-pattern.md to classes-best-practices.ts

---
+++ classes-best-practices.ts
@@ -0,0 +1,39 @@
+interface TraceDTO {
+  start: string;
+  last: string;
+  trace: string[];
+  raw: string;
+}
+
+class CustomError extends Error {
+
+  /**
+   * Keep constructors private. Prefer factory-functions.
+   */
+  private constructor(private readonly message: string) {
+    super(message);
+  }
+
+  /**
+   * Factory-Function: `of`
+   */
+  public of(message: string, code: number): CustomError {
+    const err = new CustomError(message);
+  }
+
+  /**
+   * Get a `TraceDTO` object from the call-stack. 
+   */
+  public getTrace(): TraceDTO {
+    return parseTrace(super.trace);
+  }
+}
+
+/**
+ * Keep helpers outside
+ */
+function parseTrace(traceRaw: string): TraceDTO {
+  return ...do stuff
+}
+
+export default CustomError;
